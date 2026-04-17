49975e2c99a207f802815f31fa684d36401f3814
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2026-04-17 15:50:38 -0400

    enable hatch/hatch-vcs
    Signed-off-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- pysheds/__init__.py
+++ pysheds/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "0.5"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pysheds")
+except PackageNotFoundError:
+    __version__ = "999"
+
+__all__ = [
+    # All other imports go here ...
+    "__version__",
+]
